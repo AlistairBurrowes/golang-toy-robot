abf5bccf77051eb17abc8d4051a1ccdddcdab590
Alistair Burrowes <alistair.burrowes@zendesk.com>
2021-06-11 11:52:34 +1000

    Finished writing generators

--- model/commands.go
+++ model/commands.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"fmt"
+
+	rapid "pgregory.net/rapid"
 )
 
 type CommandType int
@@ -31,3 +33,21 @@ func (c MoveCommand) String() string   { return "MOVE" }
 func (c LeftCommand) String() string   { return "LEFT" }
 func (c RightCommand) String() string  { return "RIGHT" }
 func (c ReportCommand) String() string { return "REPORT" }
+
+func GenPlaceCommand(t *rapid.T) Command {
+	return PlaceCommand{
+		Coordinate: rapid.Custom(GenCoordinate).Draw(t, "PlaceCommandPosition").(Coordinate),
+		Facing: rapid.Custom(GenDirection).Draw(t, "PlaceCommandFacing").(Direction),
+	}
+}
+
+func GenCommand(t *rapid.T) Command {
+	gen := rapid.OneOf(
+		rapid.Just(MoveCommand{}),
+		rapid.Just(LeftCommand{}),
+		rapid.Just(RightCommand{}),
+		rapid.Just(ReportCommand{}),
+		rapid.Custom(GenPlaceCommand),
+	)
+	return gen.Draw(t, "Command").(Command)
+}
